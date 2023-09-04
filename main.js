634b67ffa117fde95866cda722af7e64bd95fd21
abrantessistemas <84828246+abrantessistemas@users.noreply.github.com>
2023-09-03 21:38:36 -0300

    3 files modified

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector('#titulo').innerHTML = 'ADRIANO ABRANTES DE ANDRADE';
+document.querySelector('#boas-vindas').innerHTML =
+  'Desenvolver soluções de média e alta complexidade, com baixo nível de supervisão e pouco apoio, orientar e trabalhar com o time de desenvolvimento para encontrar soluções de negócio, manter a qualidade do software. Detalhar requisitos com o cliente para implementação de novas soluções.';
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector('#frase').innerHTML =
+  'Métodologias ágeis de desenvolvimento de software.';
