3d2c650aa65780aa5c01289ed9c74eefa6d683c7
abrantessistemas <84828246+abrantessistemas@users.noreply.github.com>
2023-09-15 18:03:49 -0300

    2 files modified

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.js';
 
+function draw() {
+  var canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect(10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(30, 30, 55, 50);
+  }
+}
+
+
+
+
+
 document.querySelector('#titulo').innerHTML = 'ADRIANO ABRANTES DE ANDRADE';
 document.querySelector('#subtitulo').innerHTML =
   'Desenvolvedor de Software Pleno';
